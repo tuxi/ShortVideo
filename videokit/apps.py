65e4a018ee37e9a57ffe0d663600e8fe402e0b56
alpface <xiaoyuan1314@me.com>
2018-03-31 21:41:45 +0800

    Update dir

--- videokit/apps.py
+++ videokit/apps.py
@@ -1,12 +1,11 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
-from ShortVideo.settings import MEDIA_URL
 
 class VideokitConfig(AppConfig):
     name = 'videokit'
     DEFAULT_VIDEO_CACHE_BACKEND = 'videokit.cache.VideoCacheBackend'
-    VIDEOKIT_CACHEFILE_DIR = 'media/CACHE/videos'
-    VIDEOKIT_TEMP_DIR =  'media/videotemp'
+    VIDEOKIT_CACHEFILE_DIR = 'CACHE/videos'
+    VIDEOKIT_TEMP_DIR =  'videotemp'
     VIDEOKIT_SUPPORTED_FORMATS = ['mp4', 'ogg', 'webm']
     VIDEOKIT_DEFAULT_FORMAT = 'mp4'
